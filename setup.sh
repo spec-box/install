35ef46503fba93154731b55eeaf745caf0874dfb
pk-72 <bit@jic.ru>
2024-04-17 16:23:17 +0300

    =

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ git clone https://github.com/spec-box/web.git
 #git clone https://github.com/spec-box/sync.git 
 #git clone https://github.com/spec-box/text-parser.git
 
+export VERSION=0.0.1
+
 echo 'Running docker-compose build'
 docker-compose build
 
