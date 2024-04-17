02cb6559e343ecc4b3dbc7f92edd66159e856004
pk-72 <bit@jic.ru>
2024-04-17 16:16:12 +0300

    +Initial version of installer

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo 'Cloning repos...'
+git clone https://github.com/spec-box/api.git
+git clone https://github.com/spec-box/web.git
+#git clone https://github.com/spec-box/sync.git 
+#git clone https://github.com/spec-box/text-parser.git
+
+echo 'Running docker-compose build'
+docker-compose build
+
+
+echo 'Starting service on port 80'
+docker-compose up
